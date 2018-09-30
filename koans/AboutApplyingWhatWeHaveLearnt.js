eca3efd413a14b2b8f1f51c9b6112a4183c7eeda
Merary Martinez-Cobian <40481640+MeraryMC@users.noreply.github.com>
2018-09-29 19:56:14 -0700

    Final - All Tests Passing

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -95,7 +95,6 @@ describe("About Applying What We Have Learnt", function() {
 
 //   it("should find the smallest number divisible by each of the numbers 1 to 20", function () {
       
-    
 //   });
 
 //   it("should find the difference between the sum of the squares and the square of the sums", function () {
